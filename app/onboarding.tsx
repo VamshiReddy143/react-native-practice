429ee259428d1aa129fbef919991debca9bafb2d
vamshi <vamshireddy0726@gmail.com>
2026-03-28 19:26:23 +0530

    started

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const onboarding = () => {
+  return (
+    <View className='flex items-center justify-center bg-background flex-1'>
+      <Text className='text-xl font-bold text-primary'>Onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
